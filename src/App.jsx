3a0bf6d22580ee108ed22187576915050f25b53d
Polinasateesh <polinasateesh111@gmail.com>
2023-11-06 21:42:08 +0530

    commits

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+
 import { useState } from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.css";
@@ -5,6 +6,7 @@ import Col from "react-bootstrap/Col";
 import Container from "react-bootstrap/Container";
 import Image from "react-bootstrap/Image";
 import Row from "react-bootstrap/Row";
+import Button from "react-bootstrap/Button";
 import img1 from "./assets/image-1.png";
 import img2 from "./assets/image-2.png";
 import img3 from "./assets/image-3.png";
@@ -16,103 +18,145 @@ import img8 from "./assets/image-8.png";
 import img9 from "./assets/image-9.png";
 import img10 from "./assets/image-10.png";
 import img11 from "./assets/image-11.png";
-import Button from 'react-bootstrap/Button';
-import 'react-toastify/dist/ReactToastify.css';
-import { ToastContainer, toast } from 'react-toastify';
+import gallery from "./assets/gallery.png"
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlus } from '@fortawesome/free-solid-svg-icons';
 
 const App = () => {
-  // Define the initial state for images
-  const [images, setImages] = useState([
-    { id: 1, url: img1, isSelected: false },
-    { id: 2, url: img2, isSelected: false },
-    { id: 3, url: img3, isSelected: false },
-    { id: 4, url: img4, isSelected: false },
-    { id: 5, url: img5, isSelected: false },
-    { id: 6, url: img6, isSelected: false },
-    { id: 7, url: img7, isSelected: false },
-    { id: 8, url: img8, isSelected: false },
-    { id: 9, url: img9, isSelected: false },
-    { id: 10, url: img10, isSelected: false },
-    { id: 11, url: img11, isSelected: false }
-  ]);
+  // Your existing code here...
 
-  // Define state for the count of selected images
-  const [selectedImagesCount, setSelectedImagesCount] = useState(0)
+  const defaultImages=[
+        { id: 1, url: img11, isSelected: false },
+        { id: 2, url: img3, isSelected: false },
+        { id: 3, url: img2, isSelected: false },
+        { id: 4, url: img7, isSelected: false },
+        { id: 5, url: img5, isSelected: false },
+        { id: 6, url: img9, isSelected: false },
+        { id: 7, url: img10, isSelected: false },
+        { id: 8, url: img4, isSelected: false },
+        { id: 9, url: img8, isSelected: false },
+        { id: 10, url: img1, isSelected: false },
+        { id: 12, url: img6, isSelected: false },
+        {id:13,url:gallery,isSelected:false}
+      ]
+      // Define the initial state for images
+      const [images, setImages] = useState(defaultImages);
+    
+      // Define state for the count of selected images
+      const [selectedImagesCount, setSelectedImagesCount] = useState(0)
+    
+      // Function to toggle the selection of an image
+      const toggleSelect = (id) => {
+        console.log(id);
+        const updatedImages = images.map((img) =>
+          img.id === id ? { ...img, isSelected: !img.isSelected } : img
+        );
+        setImages(updatedImages);
+    
+        // Count the selected images
+        const filteredImages = updatedImages.filter((eachImageSelection) => eachImageSelection.isSelected)
+        setSelectedImagesCount(filteredImages.length)
+      };
+    
+      // Function to delete selected images
+      const deleteSelected = () => {
+        if(selectedImagesCount > 0){
+          // Display a success toast with the count of deleted files
+          toast.success(`${selectedImagesCount} ${selectedImagesCount > 1 ? "Files" : "File"} Deleted Successfully`, {
+            position: toast.POSITION.TOP_RIGHT,
+            autoClose: 600,
+          })
+    
+          // Filter out the selected images from the state
+          const updatedImages = images.filter((img) => !img.isSelected);
+          setImages(updatedImages);
+          setSelectedImagesCount(0)
+        } else {
+          // Display an error toast when no files are selected
+          toast.error(' No Files are Selected', {
+            position: toast.POSITION.TOP_RIGHT,
+            autoClose: 700,
+          })
+        }
+      }
 
-  // Function to toggle the selection of an image
-  const toggleSelect = (id) => {
-    const updatedImages = images.map((img) =>
-      img.id === id ? { ...img, isSelected: !img.isSelected } : img
-    );
-    setImages(updatedImages);
-
-    // Count the selected images
-    const filteredImages = updatedImages.filter((eachImageSelection) => eachImageSelection.isSelected)
-    setSelectedImagesCount(filteredImages.length)
-  };
-
-  // Function to delete selected images
-  const deleteSelected = () => {
-    if(selectedImagesCount > 0){
-      // Display a success toast with the count of deleted files
-      toast.success(`${selectedImagesCount} ${selectedImagesCount > 1 ? "Files" : "File"} Deleted Successfully`, {
-        position: toast.POSITION.TOP_RIGHT,
-        autoClose: 600,
-      })
-
-      // Filter out the selected images from the state
-      const updatedImages = images.filter((img) => !img.isSelected);
-      setImages(updatedImages);
-      setSelectedImagesCount(0)
-    } else {
-      // Display an error toast when no files are selected
-      toast.error(' No Files are Selected', {
-        position: toast.POSITION.TOP_RIGHT,
-        autoClose: 700,
-      })
+  const onDragEnd = (result) => {
+    if (!result.destination) {
+      return;
     }
-  }
 
+    const items = Array.from(images);
+    const [reorderedItem] = items.splice(result.source.index, 1);
+    items.splice(result.destination.index, 0, reorderedItem);
 
+    setImages(items);
+  };
 
   return (
     <>
-     <ToastContainer />
+      <ToastContainer />
       <div className="d-flex justify-content-between align-items-center m-1">
-        <div>
-          <input type="checkbox" className="input-checkbox" style={{ transform: "scale(1.2)" }} checked={selectedImagesCount > 0 ? true : false}/>{" "}
-          <label className="label">{selectedImagesCount}{" "}{selectedImagesCount > 1 ? "Files Selected" : "File selected"}</label>
-        </div>
-        <div>
-          <Button variant="outline-danger" className="__button" onClick={deleteSelected}>Delete Files</Button>
+      <div>
+           <input type="checkbox" className="input-checkbox" style={{ transform: "scale(1.2)" }} checked={selectedImagesCount > 0 ? true : false}/>{" "}
+           <label className="label">{selectedImagesCount}{" "}{selectedImagesCount > 1 ? "Files Selected" : "File selected"}</label>
+         </div>
+         <div>
+           <Button variant="outline-danger" className="__button" onClick={deleteSelected}>Delete Files</Button>
         </div>
       </div>
-      <hr/>
+      <hr />
       <Container fluid>
-        <Row>
-          {images.map((eachImage, index) => (
-            <Col xs={12} md={6} lg={4} xl={3} key={eachImage.id}>
-              <div className={`image-container ${index===0?"size":""} ${eachImage.isSelected ? 'dull' : ''}`} >
-                <input
-                  type="checkbox"
-                  onChange={() => toggleSelect(eachImage.id)}
-                  checked={eachImage.isSelected}
-                  style={{ transform: "scale(1.2)" }}
-                />
-                <Image
-                  src={eachImage.url}
-                  onClick={() => toggleSelect(eachImage.id)}
-                  width={"100%"}
-                  alt={eachImage.url}
-
-                />
-              </div>
-            </Col>
-          ))}
-        </Row>
+        <DragDropContext onDragEnd={onDragEnd}>
+          <Droppable droppableId="gallery">
+            {(provided) => (
+              <Row
+                {...provided.droppableProps}
+                ref={provided.innerRef}
+              >
+                {images.map((eachImage, index) => (
+                  <Draggable key={eachImage.id} draggableId={String(eachImage.id)} index={index}>
+                    {(provided) => (
+                      <Col
+                        xs={12}
+                        md={6}
+                        lg={4}
+                        xl={3}
+                        ref={provided.innerRef}
+                        {...provided.draggableProps}
+                        {...provided.dragHandleProps}
+                      >
+                        <div className={`image-container ${index === 0 ? "size" : ""} ${eachImage.isSelected ? 'dull' : ''} ${eachImage.id===13?"add-image":''}`}>
+                          <input
+                            type="checkbox"
+                            onChange={() => toggleSelect(eachImage.id)}
+                            checked={eachImage.isSelected}
+                            className={eachImage.id===13?"add-displayNone":''}
+                            style={{ transform: "scale(1.2)" }}
+                          />
+                          <Image
+                            src={eachImage.url}
+                            onClick={() => toggleSelect(eachImage.id)}
+                            width={eachImage.id===13?"40px":"100%"}
+                            alt={eachImage.url}
+                          />
+                          {eachImage.id===13&&<p>Add Image</p>}
+                        </div>
+                      </Col>
+                    )}
+                  </Draggable>
+                ))}
+                {provided.placeholder}
+              </Row>
+            )}
+          </Droppable>
+        </DragDropContext>
       </Container>
     </>
   );
 };
 
 export default App;
+
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import { DndProvider } from 'react-dnd';
+import { HTML5Backend } from 'react-dnd-html5-backend';
 
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <DndProvider backend={HTML5Backend}>
+      <App />
+    </DndProvider>
   </React.StrictMode>,
-)
+  document.getElementById('root')
+);
+
